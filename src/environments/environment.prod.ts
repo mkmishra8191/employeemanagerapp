56956c70c3503aaeb98aff2a1150470b3679f973
manjeeteng8191 <manjeetarmy8191@gmail.com>
2021-03-09 14:31:29 +0530

    first committ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://3.140.203.231:8080'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiBaseUrl: 'http://3.140.203.231:8080'
 };
 
