113d16e1744d1ad27648bc25d4796eef9360d609
manjeeteng8191 <manjeetarmy8191@gmail.com>
2021-03-10 16:19:28 +0530

    kkkkkk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://ec2-3-141-172-241.us-east-2.compute.amazonaws.com:8080'
+  apiBaseUrl: 'http://ec2-3-141-56-206.us-east-2.compute.amazonaws.com:8080'
 };
